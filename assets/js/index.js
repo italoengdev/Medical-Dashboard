d48ba9c9095eb9ec4766b7012c9fc5cd542bb0a0
italoengdev <italo_saraiva182@hotmail.com>
2022-05-23 16:02:00 -0300

    Second attempt

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+function myFunction(x) {
+  if (x.matches) {
+    // If media query matches
+    document.body.style.backgroundColor = 'yellow'
+  } else {
+    document.body.style.backgroundColor = 'pink'
+  }
+}
+
+var x = window.matchMedia('(max-width: 700px)')
+myFunction(x) // Call listener function at run time
+// Attach listener function on state changes
