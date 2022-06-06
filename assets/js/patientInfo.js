32f51fc262b4751ca3ebafde55a3957dc8c5db7f
italoengdev <italo_saraiva182@hotmail.com>
2022-06-06 10:41:35 -0300

    final-version-left-documentation

--- assets/js/patientInfo.js
+++ assets/js/patientInfo.js
@@ -1,3 +1,164 @@
+console.log('Searching pacients...')
+
+var abc = new XMLHttpRequest()
+
+var edf = new XMLHttpRequest()
+
+abc.open('GET', 'https://cm42-medical-dashboard.herokuapp.com/patients')
+edf.open('GET', 'https://cm42-medical-dashboard.herokuapp.com/appointments')
+
+abc.addEventListener('load', () => {
+  if (abc.status == 200) {
+    var response = abc.responseText
+    var pacients = JSON.parse(response)
+    objectSelectedByName = findByName(pacients, data)
+    console.log(objectSelectedByName)
+    //Picking Patient Info
+    document.getElementById('pickName').textContent = sessionStorage.getItem('chave')
+    document.getElementById('pickCpf').textContent = validaCpf(
+      objectSelectedByName.document
+    )
+    document.getElementById('pickAge').textContent =
+      calcAge(objectSelectedByName.birthday) + 'y/o'
+    // Patient Info
+    // Plan Information
+    document.getElementById('pickHealthPlan').textContent =
+      objectSelectedByName.insurancePlan
+
+    document.getElementById('pickPlanId').textContent =
+      objectSelectedByName.healthSystemId
+  }
+})
+abc.send(), { once: true }
+// Saving local id to relate with other array
+
+
+
+edf.addEventListener('load', () => {
+  if (edf.status == 200) {
+    var response = edf.responseText
+
+    var appointments2 = JSON.parse(response)
+
+    // Extracted Data from json-server
+    console.log(appointments2)
+    // Extracted Data from json-server
+    selectedAttById = findAppointmentAtt(appointments2)
+
+    sortBy(selectedAttById, { prop: 'startTime' })
+    console.log(selectedAttById)
+
+    lastSelectedAttById = selectedAttById[selectedAttById.length - 1]
+
+    document.getElementById('pickTypeDoc').textContent =
+      lastSelectedAttById.specialty
+
+    arrangeDate =
+      lastSelectedAttById.startTime.substr(8, 2) +
+      '/' +
+      lastSelectedAttById.startTime.substr(5, 2) +
+      '/' +
+      lastSelectedAttById.startTime.substr(0, 4)
+
+    document.getElementById('pickDateApp').textContent = arrangeDate
+
+    document.getElementById('pickDateApp2').textContent = arrangeDate
+
+    document.getElementById('pickTypeDoc2').textContent =
+      lastSelectedAttById.specialty + ' ' + 'says:'
+
+    document.getElementById('pickAppDescription').textContent =
+      lastSelectedAttById.description
+
+    //taking the names by the other Json-Server and replacing them with your id
+    // History Table Patient
+    function montaTr(paciente) {
+      var pacienteTr = document.createElement('tr')
+      pacienteTr.classList.add('appointment-recent')
+      pacienteTr.appendChild(
+        montaTd(
+          paciente.startTime.substr(8, 2) +
+            '/' +
+            paciente.startTime.substr(5, 2) +
+            '/' +
+            paciente.startTime.substr(0, 4) +
+            ' ' +
+            paciente.startTime.substr(11, 5) +
+            ' ' +
+            paciente.startTime.substr(35, 5),
+          'info-start'
+        )
+      )
+      pacienteTr.appendChild(montaTd(paciente.type, 'info-type'))
+
+      pacienteTr.appendChild(montaTd(paciente.status, 'info-status'))
+
+      return pacienteTr
+    }
+
+    function adicionaPacienteNaLista(paciente, id) {
+      var pacienteTr = montaTr(paciente)
+      var tdDateYear = document.querySelector(id)
+      tdDateYear.appendChild(pacienteTr)
+    }
+
+    function montaTd(dado, classe) {
+      var tdDate = document.createElement('td')
+      tdDate.textContent = dado
+      tdDate.classList.add(classe)
+
+      return tdDate
+    }
+
+    selectedAttById.forEach(function (paciente) {
+      adicionaPacienteNaLista(paciente, '#table-pacient-history')
+    })
+
+    // History Table Patient
+    // Recent Table Patient
+    const filterByExpiration = () =>
+      selectedAttById.filter(
+        ({ startTime }) => new Date(startTime) <= new Date()
+      )
+    console.log(filterByExpiration())
+
+    filterByExpiration().forEach(function (paciente) {
+      adicionaPacienteNaLista(paciente, '#table-pacient-recent')
+    })
+
+    // Recent Table Patient
+    // Upcoming Table Patient
+    const filterByFuture = () =>
+      selectedAttById.filter(
+        ({ startTime }) => new Date(startTime) > new Date()
+      )
+    console.log(filterByFuture())
+
+    filterByFuture().forEach(function (paciente) {
+      adicionaPacienteNaLista(paciente, '#table-pacient-upcoming')
+    })
+
+    // Upcoming Table Patient
+    // Conditional Color for Status
+    var trTags = document.getElementsByTagName('tr')
+    for (var i = 0; i < trTags.length; i++) {
+      var tdFourthEl = trTags[i].children[2] // starts with 0, so 3 is the 4th element
+      if (tdFourthEl.innerText === 'absent') {
+        tdFourthEl.style.backgroundColor = 'red'
+      } else if (tdFourthEl.innerText === 'completed') {
+        tdFourthEl.style.backgroundColor = 'green'
+      } else if (tdFourthEl.innerText === 'cancelled') {
+        tdFourthEl.style.backgroundColor = 'grey'
+      }
+    }
+    // Conditional Color for Status
+  }
+})
+edf.send(), { once: true }
+
+// Getting the Storage data
+
+// Getting the Storage data
 
 function findByName(source, name) {
   for (var i = 0; i < source.length; i++) {
@@ -27,25 +188,17 @@ function validaCpf(cpf) {
 }
 
 function findAppointmentAtt(people) {
-  return people
+    return people
     .filter(function (p) {
-      return p.patientId === parseInt(data1)
+      return p.patientId === parseInt(sessionStorage.getItem('id'))
     })
     .map(function (p) {
       return p
     })
-}
+  }
+  
 
-// function findAppointmentRecent(people) {
-//   var today = new Date()
-//   return people
-//     .filter(function (p) {
-//       return p.startTime > today
-//     })
-//     .map(function (p) {
-//       return p
-//     })
-// }
+
 
 // SORTBY FUNCTION
 var sortBy = (function () {
@@ -95,177 +248,3 @@ function sortByDate(arr) {
 
   return arr
 }
-
-
-console.log('Searching pacients...')
-//b.setAttribute('disabled', 'disabled')
-
-var abc = new XMLHttpRequest()
-var edf = new XMLHttpRequest()
-
-abc.open('GET', 'https://cm42-medical-dashboard.herokuapp.com/patients')
-edf.open('GET', 'https://cm42-medical-dashboard.herokuapp.com/appointments')
-
-abc.addEventListener('load', function () {
-  if (abc.status == 200) {
-    var response = abc.responseText
-    var pacients = JSON.parse(response)
-    objectSelectedByName = findByName(pacients, data)
-    window.teste1 = objectSelectedByName.id
-    console.log(window.teste1);
-    sessionStorage.setItem('id', objectSelectedByName.id)
-    console.log(objectSelectedByName)
-    // Patient Info
-    document.getElementById('pickCpf').textContent = validaCpf(
-      objectSelectedByName.document
-    )
-    document.getElementById('pickAge').textContent =
-      calcAge(objectSelectedByName.birthday) + 'y/o'
-    // Patient Info
-    // Plan Information
-    document.getElementById('pickHealthPlan').textContent =
-      objectSelectedByName.insurancePlan
-
-    document.getElementById('pickPlanId').textContent =
-      objectSelectedByName.healthSystemId
-  }
-})
-abc.send(), { once: true }
-// Saving local id to relate with other array
-
-edf.addEventListener('load', function () {
-  if (edf.status == 200) {
-    var response = edf.responseText
-
-    var appointments2 = JSON.parse(response)
-
-    // Extracted Data from json-server
-    console.log(appointments2)
-    // Extracted Data from json-server
-
-    selectedAttById = findAppointmentAtt(appointments2)
-
-    sortBy(selectedAttById, { prop: 'startTime' })
-    console.log(selectedAttById)
-
-    lastSelectedAttById = selectedAttById[selectedAttById.length - 1]
-
-    document.getElementById('pickTypeDoc').textContent =
-      lastSelectedAttById.specialty
-
-    arrangeDate =
-      lastSelectedAttById.startTime.substr(8, 2) +
-      '/' +
-      lastSelectedAttById.startTime.substr(5, 2) +
-      '/' +
-      lastSelectedAttById.startTime.substr(0, 4)
-
-    document.getElementById('pickDateApp').textContent = arrangeDate
-
-    document.getElementById('pickDateApp2').textContent = arrangeDate
-
-    document.getElementById('pickTypeDoc2').textContent =
-      lastSelectedAttById.specialty + ' ' + 'says:'
-
-    document.getElementById('pickAppDescription').textContent =
-      lastSelectedAttById.description
-
-    //taking the names by the other Json-Server and replacing them with your id
-
-    // History Table Patient
-
-    function montaTr(paciente) {
-      var pacienteTr = document.createElement('tr')
-      pacienteTr.classList.add('appointment-recent')
-      pacienteTr.appendChild(
-        montaTd(
-          paciente.startTime.substr(8, 2) +
-            '/' +
-            paciente.startTime.substr(5, 2) +
-            '/' +
-            paciente.startTime.substr(0, 4) +
-            ' ' +
-            paciente.startTime.substr(11, 5) +
-            ' ' +
-            paciente.startTime.substr(35, 5),
-          'info-start'
-        )
-      )
-      pacienteTr.appendChild(montaTd(paciente.type, 'info-type'))
-
-      pacienteTr.appendChild(montaTd(paciente.status, 'info-status'))
-
-      return pacienteTr
-    }
-
-    function adicionaPacienteNaLista(paciente,id) {
-      var pacienteTr = montaTr(paciente)
-      var tdDateYear = document.querySelector(id)
-      tdDateYear.appendChild(pacienteTr)
-    }
-
-    function montaTd(dado, classe) {
-      var tdDate = document.createElement('td')
-      tdDate.textContent = dado
-      tdDate.classList.add(classe)
-
-      return tdDate
-    }
-
-
-    selectedAttById.forEach(function (paciente) {
-      adicionaPacienteNaLista(paciente,'#table-pacient-history')
-    })
-
-    // History Table Patient
-
-    // Recent Table Patient
-
-    const filterByExpiration = () => selectedAttById.filter(({ startTime }) => new Date(startTime) <= new Date());
-    console.log(filterByExpiration());
-
-    filterByExpiration().forEach(function (paciente) {
-      adicionaPacienteNaLista(paciente,'#table-pacient-recent')
-    })
-
-
-    // Recent Table Patient
-
-    // Upcoming Table Patient
-    const filterByFuture = () => selectedAttById.filter(({ startTime }) => new Date(startTime) > new Date());
-    console.log(filterByFuture());
-
-    filterByFuture().forEach(function (paciente) {
-      adicionaPacienteNaLista(paciente,'#table-pacient-upcoming')
-    })
-
-    // Upcoming Table Patient
-
- // Conditional Color for Status
- var trTags = document.getElementsByTagName('tr')
- for (var i = 0; i < trTags.length; i++) {
-   var tdFourthEl = trTags[i].children[2] // starts with 0, so 3 is the 4th element
-   if (tdFourthEl.innerText === 'absent') {
-     tdFourthEl.style.backgroundColor = 'red'
-   } else if (tdFourthEl.innerText === 'completed') {
-     tdFourthEl.style.backgroundColor = 'green'
-   } else if (tdFourthEl.innerText === 'cancelled') {
-     tdFourthEl.style.backgroundColor = 'grey'
-   }
- }
- // Conditional Color for Status
-
-
-
-
-
-
-  }
-})
-edf.send(), { once: true }
-
-// Getting the Storage data
-data1 = sessionStorage.getItem('id')
-
-console.log(data1)
-// Getting the Storage data
--- assets/js/pickEvents.js
+++ assets/js/pickEvents.js
@@ -3,24 +3,38 @@ function pickName() {
   patientName = event.target.textContent
   console.log(patientName)
   sessionStorage.setItem('chave', patientName)
-  
-  let menuAbrir = document.getElementById('pickName')
-  if (menuAbrir) {
-    menuAbrir.addEventListener('load', function () {
-      window.onload.getElementById('pickName').textContent = data
-    })
-  }
+
 }
 data = sessionStorage.getItem('chave')
-const input = document.getElementById('pickName');
-console.log(input); // 👉️ null
 
-if (input) {
-  input.textContent = data;
-}
 // Store it in localstorage
 
 // Get the value of your saved variable to use it elsewhere
 console.log(data)
 
+const url = `https://cm42-medical-dashboard.herokuapp.com/patients`
 
+    fetch(url)
+      .then(response => response.json())
+      .then(pacients => {
+        objectSelectedByNameId = findByName(pacients, data)
+        sessionStorage.setItem('id', objectSelectedByNameId.id)
+        console.log(objectSelectedByNameId)
+        console.log(sessionStorage.getItem('id'));
+      })
+
+      
+
+// Saving local id to relate with other array
+
+
+
+
+function findByName(source, name) {
+  for (var i = 0; i < source.length; i++) {
+    if (source[i].name === name) {
+      return source[i]
+    }
+  }
+  throw "Couldn't find object with id: " + name
+}
